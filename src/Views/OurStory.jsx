cc8bafc1ade47545d6b09c3fb9d9833f375bf84e
Sardor ibn Farhod <92437771+SardorDev12@users.noreply.github.com>
2023-10-02 21:42:34 +0500

    our story page

--- src/Views/OurStory.jsx
+++ src/Views/OurStory.jsx
@@ -1,4 +1,52 @@
+import "../styles/ourStory.scss";
+import img1 from "../assets/images/Blog Image.png";
+import img2 from "../assets/images/Blog Image.png";
+
 function OurStory() {
-  return <div className="page">OurStory</div>;
+  return (
+    <div className="ourstory page">
+      <div className="story-image">
+        <h1 className="section-title">Our Story</h1>
+        <p className="story-parag">
+          Through True Rich Attended does no end it his mother since favourable
+          real had half every him case in packages enquire we up ecstatic.
+        </p>
+        <div className="images">
+          <div className="image">
+            <img src={img1} alt="image" />
+          </div>
+          <div className="image">
+            <img src={img2} alt="image" />
+          </div>
+        </div>
+      </div>
+      <div className="story-text">
+        <h1 className="story-text__title">
+          One of the best cafes in <span>New York.</span>
+        </h1>
+        <p>
+          Writing UX copies can be a little frustrating and confusing, and
+          sometimes we are unsure about how to get the right word. To crack the
+          code for the UX copies, we at Zeta Design wanted to build a Figma
+          plugin for the larger design community. The plugin is called the Ghost
+          UXWriter and has a set of UX copies cataloged and categorized with a
+          voice and tone variation ranging from plain, casual to playful. The
+          intention to build this Figma plugin originated from our Medium blog
+          post, Designing voice and tone for error messages.
+        </p>
+        <br />
+        <p>
+          Writing UX copies can be a little frustrating and confusing, and
+          sometimes we are unsure about how to get the right word. To crack the
+          code for the UX copies, we at Zeta Design wanted to build a Figma
+          plugin for the larger design community. The plugin is called the Ghost
+          UXWriter and has a set of UX copies cataloged and categorized with a
+          voice and tone variation ranging from plain, casual to playful. The
+          intention to build this Figma plugin originated from our Medium blog
+          post, Designing voice.
+        </p>
+      </div>
+    </div>
+  );
 }
 export default OurStory;
