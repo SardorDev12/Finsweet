99ec3e1b2321e009243c346464dbef8024fa3500
Sardor ibn Farhod <92437771+SardorDev12@users.noreply.github.com>
2023-10-02 12:45:40 +0500

    test data

--- src/Layouts/GlobalContext.jsx
+++ src/Layouts/GlobalContext.jsx
@@ -1,10 +1,4 @@
-import {
-  Children,
-  createContext,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import { createContext, useContext } from "react";
 import data from "../../data/db.json";
 const DataContext = createContext();
 
@@ -13,17 +7,5 @@ export const useDataContext = () => {
 };
 
 export const DataProvider = ({ children }) => {
-  // const [data, setData] = useState([]);
-
-  // // const fetchData = async () => {
-  // //   const res = await fetch("../../data/db.json");
-  // //   const data = await res.json();
-  // //   setData(data);
-  // // };
-  // useEffect(() => {
-  //   // fetchData();
-  //   setData(data);
-  // }, []);
-
   return <DataContext.Provider value={data}>{children}</DataContext.Provider>;
 };
